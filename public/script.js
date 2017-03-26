60c034cae8839b680621481c4cf99681c865e6ad
klambo94 <klambo94@gmail.com>
2017-03-26 17:35:59 +0000

    Added more functionality to each page

--- public/script.js
+++ public/script.js
@@ -30,18 +30,26 @@ spaApp.config(function($routeProvider) {
 spaApp.controller('mainController', function($scope) {
 
 	//create a message to display in our view
-	$scope.message = 'Everyone come and see how good I look!';
+	$scope.message = 'Kendra Lamb - Student - Intern';
 });
 
 //create the controller and inject angular's $scope
 spaApp.controller('aboutController', function($scope) {
 
 	//create a message to display in our view
-	$scope.message = 'I am about page!';
+	$scope.message = 'More about Kendra Lamb';
+	$scope.aboutMessage = "I am currently finishing my Junior year at Metropolitan State University at Denver for a Bacherlors Degree in Computer Science." + 
+							"I also am an intern at Recondo Technology for the Bots team. There I work on maintaing and creating webbots to help collect insurance information" +
+							"from insurace companies to return to hospitals on a patient by patient basis. More specifically dealing with Authorizations, Claims, and Eligibiliy benfeits."
 });
 
 spaApp.controller('contactController', function($scope) {
 
 	//create a message to display in our view
-	$scope.message = 'I am contact page!';
+	$scope.message = 'Ways to contact Kendra Lamb';
+	$scope.linkedinMsg = "Linked in Profile";
+	$scope.emailMsg = 'Email: ';
+	$scope.email = 'klambo94@gmail.com';
+	$scope.phoneMsg = 'Phone: '
+	$scope.phone = '720-231-8807';
 });
